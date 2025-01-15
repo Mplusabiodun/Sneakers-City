4bbd0d6e2447d845c43edc0c0bbbf14b9e53e8cb
Fatai Abiodun M. <fataimonsuru2020@gmail.com>
2025-01-15 10:48:19 +0100

    creating my data file

---
+++ data.js
@@ -0,0 +1,47 @@
+export const sneakers = [
+  {
+    image: "src/assets/jordan_delta2.png",
+    brand: "Jordan Delta 2",
+    price: 78000,
+  },
+  {
+    image: "src/assets/nike_air_jordanXT.png",
+    brand: "Nike Air Jordan XT",
+    price: 35000,
+  },
+  {
+    image: "src/assets/rx_intl_game.png",
+    brand: "RS-X-INTL Game",
+    price: 35000,
+  },
+  {
+    image: "src/assets/suedeclassic.png",
+    brand: "Suede Classic",
+    price: 35000,
+  },
+  {
+    image: "src/assets/nmd_refined.png",
+    brand: "NMD 1 Refined",
+    price: 35000,
+  },
+  {
+    image: "src/assets/superstar_classic.png",
+    brand: "Superstar Classic",
+    price: 35000,
+  },
+  {
+    image: "src/assets/adiddas_beluga.png",
+    brand: "Adiddas Beluga",
+    price: 35000,
+  },
+  {
+    image: "src/assets/nike_air_jirdan.png",
+    brand: "Nike Air Jordan XT",
+    price: 35000,
+  },
+  {
+    image: "src/assets/nike_wobler.png",
+    brand: "Nike Wobler",
+    price: 35000,
+  },
+];
---
+++ src/components/Home.jsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <h2>This is homepage</h2>;
+}
