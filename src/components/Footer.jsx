b6f681e2ec398aa5d4ca4549cf8503bdca1214d8
Fatai Abiodun M. <fataimonsuru2020@gmail.com>
2025-01-24 13:00:33 +0100

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,27 @@
+import sneakerLogo from "../assets/logo.png";
 import twitter from "../assets/twitter_bird.png";
 import instagram from "../assets/instagram.png";
 import facebook from "../assets/facebook.png";
 
+import classes from "./Footer.module.css";
+
 export default function Footer() {
   return (
-    <footer>
+    <footer className={classes.footer}>
       <hr />
-      <div>
-        <img alt="Snaeker logo" />
+      <div className={classes.logo}>
+        <img src={sneakerLogo} alt="Snaeker logo" />
         <h2>SC.</h2>
       </div>
-      <p>
+      <p className={classes.footerwords}>
         We don't just sell shoes, we sell memories and collectibles. We collect
         the best in the best with an attention to all little details. We know
         that shoes speaks louder than words that's why we've mastered the
         science of god sneakers.
       </p>
-      <div>
-        <div>
-          <p>Don't miss out-on-once-in-a-while-deals</p>
+      <div className={classes.dontmiss_support_copyright}>
+        <div className={classes.dontmiss}>
+          <p>Don't miss out-on-once-in-a-while-deals:</p>
           <ul>
             <li>
               <img src={twitter} alt="twitter" />
@@ -31,6 +34,8 @@ export default function Footer() {
             </li>
           </ul>
         </div>
+        <p>Support line: +250 788 467 808</p>
+        <p>Copyright 2021 Sneaker City ltd</p>
       </div>
     </footer>
   );
